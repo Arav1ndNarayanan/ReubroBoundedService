057501afa1fb9461adf30dc238104df16456efe3
AravattaN <ampadiaravindam@gmail.com>
2020-10-06 11:43:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/reubroboundedservice/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.reubroboundedservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button start = findViewById(R.id.startService);
+        Button stop =  findViewById(R.id.stopservice);
+        start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
